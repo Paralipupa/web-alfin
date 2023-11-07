ad1f83e5eb85ab44fc5d95afb8d4da4ef7d316b9
paralipupa <a917kk@mail.ru>
2023-11-07 16:17:57 +0300

    ЦБ

--- web/routes.py
+++ web/routes.py
@@ -15,43 +15,50 @@ from .settings import *
 
 @auth.verify_password
 def verify_password(username, password):
-    if username in users and \
-            check_password_hash(users.get(username), password):
+    if username in users and check_password_hash(users.get(username), password):
         return username
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 @auth.login_required
 def index():
-    form : UploadForm = UploadForm()
-    if request.method == 'POST': # and form.validate_on_submit():
-        return __report(form.date_purpose, form.is_archi.data)
-    return render_template('upload_multi.html', title='Отчеты', form=form)
+    form: UploadForm = UploadForm()
+    if request.method == "POST":  # and form.validate_on_submit():
+        responce = __report(form.date_purpose, form.is_archi.data)
+        return responce
+    return render_template("upload_multi.html", title="Отчеты", form=form)
+
 
 def __report(date_purpose: DateField, is_archi: bool = False):
     files = __upload_file()
     if files:
         if date_purpose.raw_data[0]:
-            date_p = datetime.strptime(date_purpose.raw_data[0],"%Y-%m-%d").date()
+            date_p = datetime.strptime(date_purpose.raw_data[0], "%Y-%m-%d").date()
         else:
-            date_p = None            
-        report = Calc(files,  date_p, is_archi)
+            date_p = None
+        report = Calc(files, date_p, is_archi)
         return __download_file(report.run())
-    return redirect('/')
+    return redirect("/")
+
 
 def __upload_file() -> str:
     files = []
     if request.files:
-        os.makedirs(app.config['UPLOAD_DIR'], exist_ok=True)
-        for index, file in enumerate(request.files.getlist('file')):
+        os.makedirs(app.config["UPLOAD_DIR"], exist_ok=True)
+        for index, file in enumerate(request.files.getlist("file")):
             if file.filename:
                 filename = pathlib.Path(
-                    app.config['UPLOAD_DIR'], secure_filename(f'{index}_{file.filename}'))
+                    app.config["UPLOAD_DIR"],
+                    secure_filename(f"{index}_{file.filename}"),
+                )
                 file.save(filename)
                 files.append(str(filename))
     return files
 
+
 def __download_file(filename: str):
     if filename is None:
-        return redirect('/')
-    return send_from_directory(os.path.dirname(filename), os.path.basename(filename), as_attachment=True)
-
+        return redirect("/")
+    return send_from_directory(
+        os.path.dirname(filename), os.path.basename(filename), as_attachment=True
+    )
